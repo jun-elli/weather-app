3ef96fb18854a72a52a0eab69e80509b96bcb101
Jun <jun279r@gmail.com>
2022-05-26 17:16:19 +0200

    Create navbar with 2 buttons

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,9 +2,24 @@ import { useState } from "react";
 
 function SideNav() {
     return (
-        <div>
-            I'm a side navbar!
-        </div>
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark" >
+            <div className="container-fluid" >
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                    <ul class="navbar-nav gap-3">
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-secondary">Today's weather</button>
+                        </li>
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-primary">Next 5 days</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 };
 
