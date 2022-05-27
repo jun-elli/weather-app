bb7ae95b6d027bbbffa3c28adc3ef965a1f990df
Jun <jun279r@gmail.com>
2022-05-27 16:28:42 +0200

    Set location state in App from input in child component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import SideNav from './components/SideNav';
-import Search from './components/Search';
-import Today from './components/Today';
-import FiveDays from './components/FiveDays';
-import { useState } from 'react';
+import "./App.css";
+import SideNav from "./components/SideNav";
+import Search from "./components/Search";
+import Today from "./components/Today";
+import FiveDays from "./components/FiveDays";
+import { useState } from "react";
 
 function App() {
   const [isToday, setIsToday] = useState(true);
-  
-  const handleSet = (boolean) => {
-    setIsToday(boolean)
+  const [location, setLocation] = useState("");
+
+  //change which component is shown
+  const handleSetToday = (boolean) => {
+    setIsToday(boolean);
   };
-  
+
   return (
     <div className="bg-light text-align">
-      <SideNav isToday={isToday} setIsToday={handleSet} />
-      <Search />
+      <SideNav isToday={isToday} setIsToday={handleSetToday} />
+      <Search setLocation={setLocation} />
       <Today isToday={isToday} />
       <FiveDays isToday={isToday} />
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,40 @@
 import "../App.css";
 import { useState } from "react";
 
-function Search() {
-    return (
-        <div className="container mt-3">
-            <form class="col justify-content-center align-items-center input-group" >
-                <input type="text" class="form-control ms-4 bg-white input-sm" name="location" id="location" aria-describedby="helpId" placeholder="Enter your location" />
-                <button type="submit" class="btn btn-info">Go!</button>
-            </form>
-        </div>      
-    )
-};
+function Search({ setLocation }) {
+  const [input, setInput] = useState("");
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // send to parent
+    setLocation(input);
+    //reset input field
+    setInput("");
+  };
+
+  return (
+    <div className="container mt-3">
+      <form class="col justify-content-center align-items-center input-group">
+        <input
+          type="text"
+          class="form-control ms-4 bg-white input-sm"
+          name="location"
+          id="location"
+          aria-describedby="helpId"
+          placeholder="Enter your location"
+          value={input}
+          onChange={handleOnChange}
+        />
+        <button type="submit" class="btn btn-info" onClick={handleSubmit}>
+          Go!
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default Search;
