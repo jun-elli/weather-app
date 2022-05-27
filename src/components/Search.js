e939363787fbdf95b88bc378d2e3c995b45f19ae
Jun <jun279r@gmail.com>
2022-05-27 12:34:50 +0200

    Center form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
+import "../App.css";
 import { useState } from "react";
 
 function Search() {
     return (
-        <div className="container-fluid text-center mt-3">
-            <form class="input-group text-center" >
-                <input type="text" class="form-control ms-4 bg-white" name="location" id="location" aria-describedby="helpId" placeholder="Enter your location" />
+        <div className="container mt-3">
+            <form class="col justify-content-center align-items-center input-group" >
+                <input type="text" class="form-control ms-4 bg-white input-sm" name="location" id="location" aria-describedby="helpId" placeholder="Enter your location" />
                 <button type="submit" class="btn btn-info">Go!</button>
             </form>
         </div>      
