a0d5b1912f33c3dc83ae57704010be04956f4dd8
Jun <jun279r@gmail.com>
2022-05-27 17:07:09 +0200

    Fetch data for current weather and next 5 days

--- src/App.js
+++ src/App.js
@@ -3,11 +3,42 @@ import SideNav from "./components/SideNav";
 import Search from "./components/Search";
 import Today from "./components/Today";
 import FiveDays from "./components/FiveDays";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [isToday, setIsToday] = useState(true);
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("Reus");
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [nextWeather, setNextWeather] = useState(null);
+
+  useEffect(() => {
+    getCurrentWeather();
+    getNexttWeather();
+  }, [location]);
+
+  const APIkey = "a4ffdb60cbf2f99a74e15715a1531bcc";
+
+  let getCurrentWeather = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`
+    );
+    if (response.status !== 200) {
+      throw new Error("cannot fetch data");
+    }
+    let data = await response.json();
+    setCurrentWeather(data);
+  };
+
+  let getNexttWeather = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${APIkey}`
+    );
+    if (response.status !== 200) {
+      throw new Error("cannot fetch data");
+    }
+    let data = await response.json();
+    setNextWeather(data);
+  };
 
   return (
     <div className="bg-light text-align">
